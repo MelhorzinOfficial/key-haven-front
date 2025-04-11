cb7b3289756be498dae55baa6c73e04c5bf69ab9
raposoG <gabriel.henrique7@hotmail.com>
2025-04-11 20:29:53 -0300

    refactor(routes): remove authentication route component
    - Deleted the authentication route component located at src/routes/app/authentication/route.tsx.
    - Removed the associated Route and RouteComponent definitions.
    
    This change simplifies the routing structure by eliminating the unused authentication route, improving maintainability and reducing code complexity.

--- src/routes/app/authentication/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/app/authentication')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/app/authentication"!</div>
-}
