68957bdeb1aabab4847132d829816b532faaa3fc
raposoG <gabriel.henrique7@hotmail.com>
2025-04-11 20:28:48 -0300

    feat(auth): add login functionality
    - Created a new file `login.ts` in the `src/http/auth` directory.
    - Implemented the `Login` function to handle user authentication via API.
    - Defined `LoginRequest` and `LoginResponse` interfaces for type safety.
    - Added `useLogin` hook to facilitate login mutations using React Query.
    
    This commit introduces the login functionality, allowing users to authenticate and receive a token. It enhances the authentication flow and improves the overall user experience by integrating with the existing API structure.

---
+++ src/http/auth/login.ts
@@ -0,0 +1,33 @@
+import { api } from "@/lib/api";
+import { queryClient } from "@/lib/query";
+import { queryKeys } from "@/lib/query-keys";
+import { useMutation } from "@tanstack/react-query";
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  user: {
+    id: string;
+    name: string;
+  };
+  token: string;
+  expiresIn: number;
+}
+
+export async function Login(data: LoginRequest): Promise<LoginResponse> {
+  return await api
+    .post<LoginResponse>("auth/login", {
+      json: data,
+    })
+    .json<LoginResponse>();
+}
+
+export const useLogin = () => {
+  return useMutation({
+    mutationFn: Login,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: queryKeys.auth() }),
+  });
+};
