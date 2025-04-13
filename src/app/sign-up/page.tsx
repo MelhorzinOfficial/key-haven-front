7058a318970e39a73a830abfeb84c7f70cb38acb
luizpbello <luizpaulopinto25@gmail.com>
2025-04-13 17:30:39 -0300

    feat(sign-up): enhance sign-up form with additional fields and error messages

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,11 @@
+import { SignUpForm } from "@/components/forms/signUpForm";
+
 export default function SignUp() {
   return (
-    <div>
-      <h1>O mundo é difícil. </h1>
+    <div className="relative min-h-svh w-full flex flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <SignUpForm />
+      </div>
     </div>
   );
 }
