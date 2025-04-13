c05e5a479b1def414d5dc4294d86dffae18e9e58
raposoG <gabriel.henrique7@hotmail.com>
2025-04-13 12:14:20 -0300

    feat(auth): implement SignIn functionality
    - Added SignInRequest and SignInResponse interfaces to define the structure of sign-in data.
    - Implemented the SignIn function to handle user authentication via API.
    - Utilized Cookies to store the authentication token upon successful sign-in.
    - Created a custom hook useSignIn to manage the sign-in mutation with react-query.
    
    This commit introduces a complete sign-in feature, enhancing user authentication capabilities and improving the overall user experience by securely managing tokens.

--- src/http/auth/SignIn.ts
+++ src/http/auth/SignIn.ts
@@ -2,32 +2,40 @@ import { api } from "@/lib/api";
 import { queryClient } from "@/lib/query";
 import { queryKeys } from "@/lib/query-keys";
 import { useMutation } from "@tanstack/react-query";
+import Cookies from "js-cookie";
 
-export interface LoginRequest {
+export interface SignInRequest {
   email: string;
   password: string;
 }
 
-export interface LoginResponse {
+export interface SignInResponse {
   user: {
     id: string;
     name: string;
+    email: string;
   };
   token: string;
   expiresIn: number;
 }
 
-export async function Login(data: LoginRequest): Promise<LoginResponse> {
-  return await api
-    .post<LoginResponse>("auth/login", {
+export async function SignIn(data: SignInRequest): Promise<SignInResponse> {
+  const response = await api
+    .post<SignInResponse>("auth/login", {
       json: data,
     })
     .json();
+
+  if (response.token) {
+    Cookies.set("token", response.token, { expires: response.expiresIn / 86400 });
+  }
+
+  return response;
 }
 
-export const useLogin = () => {
+export const useSignIn = () => {
   return useMutation({
-    mutationFn: Login,
+    mutationFn: SignIn,
     onSuccess: () => queryClient.invalidateQueries({ queryKey: queryKeys.auth() }),
   });
 };
